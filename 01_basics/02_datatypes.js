419df9a79a8f37ff1524422181fa9d6f5f2b4690
KrishnendraTiwari <krishnendratiwari09@gmail.com>
2026-05-16 17:03:08 +0000

    message

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";
+// treat all JS code as new and modern version
+
+// alert(3 + 3) // we are using node to run this code, so alert will not work here. It is used in browser environment.
+
+console.log(3 
+    + 
+    3) // code readability is not good in this way, so we should write code in a proper way.
+
+console.log("hello world")
+
+let name = "Badal"
+let age = 20
+let isloggrdIn = false
+
+// number => 2 to power 53
+// bigint => large number ke liye use hota hai
+
+// string => ""
+// boolean => true/false
+
+// null => standalone value
+
+// undefined => 
+// symbol => used for uniqueness 
+
+// object 
+
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
