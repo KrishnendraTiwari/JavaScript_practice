2116f8f1621538726bcd3ce23ed8b499f9f0a4e0
KrishnendraTiwari <krishnendratiwari09@gmail.com>
2026-05-17 04:39:25 +0000

    comparisons

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -41,3 +41,30 @@ let stringnum = String(somenum)
 
 console.log(typeof stringnum);
 console.log(stringnum)
+
+// ************* Operations ***********
+
+let value = 3
+let negvalue = -value
+console.log(negvalue); // -3
+
+console.log(2+2); //4
+console.log(2-2); //0
+console.log(2*2); //4
+console.log(2**3); // 2^3 2*2*2 = 8
+console.log(10/3); // 3
+console.log(10%3); // 1
+
+let str = "hello"
+let str2 = "world"
+let str3 = str + str2
+
+console.log(str3);
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+console.log(+true); //1
+console.log(+""); //0
